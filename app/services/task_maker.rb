bb3b58fe3a84ccb1322c1a2810f812953c7f408e
Gary Hepting and Laura DeWald <engineering+gary+laura@sidereel.com>
2013-08-02 15:31:37 -0700

    Fiddlin' under Christopher's guidance

--- app/services/task_maker.rb
+++ app/services/task_maker.rb
@@ -1,2 +1,10 @@
 class TaskMaker
+  def self.task_for_users(args)
+    unless
+      task = Task.create(args[:task_params])
+
+      args[:users].each do |user|
+        task.users << user
+    raise TaskMaker::CreationErrors
+  end
 end
---
+++ spec/services/task_maker_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe TaskMaker do
+  let(:task_params) {{
+                 :task_name => 'name',
+                 :task_date => Date.today(),
+                 :blockers => 'no blockers',
+                 :details => 'a task'
+                }}
+  let(:user) { User.new(:email => 'eli@somewhere.com', :password => 'something') }
+  let(:user2) { User.new(:email => 'christopher@somewhereelse.com', :password => 'somethingelse') }
+  it 'creates a task with the given params' do
+    Task.should_receive(:new).with(task_params)
+
+    described_class.task_for_users(:task_params => task_params, :users => [user])
+  end
+
+  it 'links a created task to a user' do
+    expect { described_class.task_for_users(:task_params => task_params, 
+                                            :users => [user]) }.to change(Tag, :count).by(1)
+  end
+
+  it 'works with multiple users' do
+    expect { described_class.task_for_users(:task_params => task_params, 
+                                            :users => [user,user2]) }.to change(Tag, :count).by(2)
+
+  end
+
+  it 'raises on a bad task' do
+     expect { described_class.task_for_users(:task_params => {}, 
+                                            :users => [user]) }.to raise_exception(TaskMaker::BadTask)
+
+  end
+
+  it 'passes the exception error message up from Task' do
+  end
+
+  it 'handles bad users' do
+
+  end
+end
